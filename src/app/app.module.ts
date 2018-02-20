469d16cf611b17623f7b588a9db737f0d654290d
Justyna Korczynska Kadziel <s00132250@mail.itsligo.ie>
2018-02-20 07:39:38 +0000

    Create Rooms, Rooms-List, Room-Details, Rating and Bookinf-Form Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
+import { RoomDetailsComponent } from './rooms/room-details/room-details.component';
+import { BookingFormComponent } from './rooms/room-details/booking-form/booking-form.component';
+import { RatingComponent } from './rooms/room-details/rating/rating.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    RoomsListComponent,
+    RoomDetailsComponent,
+    BookingFormComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule
